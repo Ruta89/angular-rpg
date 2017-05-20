aeaf6dbc4537f474e6f081f39079a9ae9a80784d
Justin DuJardin <justin@dujardinconsulting.com>
2017-05-20 14:42:44 -0700

    Updates

--- source/ui/material/include.js
+++
@@ -1,7 +0,0 @@
-///<reference path="../../../typings/angular2/angular2.d.ts"/>
-///<reference path="../../../typings/es6-promise/es6-promise.d.ts"/>
-function isPresent(value) {
-    return value !== undefined && value !== null;
-}
-exports.isPresent = isPresent;
-//# sourceMappingURL=include.js.map
